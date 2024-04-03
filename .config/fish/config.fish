15b681fd23729d209a1aa7f7d6c806e10980a51a
julia bresolin <julia.bresolin@jbq.global>
2024-04-03 16:19:57 -0300

    chore: remove welcome message fish terminal

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,15 @@ if status is-interactive
     
 end
 
+set fish_greeting # disable welcome message
+
 starship init fish | source
 
-set PATH /Users/julia/.nvm/versions/node/v18.17.0 $PATH
+# pnpm
+set -gx PNPM_HOME "/Users/julia/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
+
+alias proxy-cs-stage="ccsrv --node https://a1409216c1tst-admin.occa.ocs.oraclecloud.com --theme Carmen\ Steffens"
