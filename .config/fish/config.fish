30476ab807cd8c884204c97780bd339ebd0ed62b
Julia Bresolin <julia.bresolin@jbq.global>
2024-06-27 10:06:09 -0300

    chore: adjustment fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,4 +13,18 @@ if not string match -q -- $PNPM_HOME $PATH
 end
 # pnpm end
 
-alias proxy-cs-stage="ccsrv --node https://a1409216c1tst-admin.occa.ocs.oraclecloud.com --theme Carmen\ Steffens"
+alias proxy-hv-stage-taqi="ccsrv --node https://a7799514c1tst-store.occa.ocs.oraclecloud.com --theme Taqi"
+alias proxy-hv-stage-iplace="ccsrv --node https://a7799514c1tst-store.occa.ocs.oraclecloud.com --theme IPlace"
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+
+# java
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home
+
+# android studio
+set --export ANDROID_HOME "$HOME/Library/Android/sdk"
+set --export PATH $ANDROID_HOME/emulator $PATH
+set --export PATH $ANDROID_HOME/platform-tools $PATH
+
